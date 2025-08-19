767e9b325bae6372412da2b4ffdf768396859892
AllenGleeson <allen_gleeson@hotmail.com>
2025-08-19 21:03:29 +0100

    Renamed ProductThread to DrawerThread and adjusted calc method

--- DrawerThread.java
+++ DrawerThread.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
+
 public class DrawerThread implements Runnable {
     // Data members
     private int sum;
     private int max;
     private int min;
     private int avg;
-    Product[] products;
+    ArrayList drawer;
 
     // Getters
     public int getSum() {
@@ -24,14 +26,14 @@ public class DrawerThread implements Runnable {
     }
 
     // Constructor
-    public DrawerThread(Product[] products) {
-        this.products = products;
+    public DrawerThread(ArrayList drawer) {
+        this.drawer = drawer;
         this.sum = 0;
         this.max = 0;
         this.min = 0;
         this.avg = 0;
     }
-    
+
     @Override
     public void run() {
         System.out.println("New DrawerThread started.");
@@ -42,23 +44,24 @@ public class DrawerThread implements Runnable {
         System.out.println("Average: " + avg);
         System.out.println("DrawerThread finished.");
     }
-    
+
+    // Question 2 - A: Calculate sum, max, min, and average
     // Method to calculate sum, max, min, and average
     private void calc() {
-        if (products == null || products.length == 0) {
-            System.out.println("No products available.");
+        if (drawer == null || drawer.size() == 0) {
+            System.out.println("No drawer records.");
             return;
         }
-        for (Product product : products) {
-            int price = product.getPrice();
-            sum += price;
-            if (price > max) {
-                max = price;
+        for (int iCount = 0; iCount < drawer.size(); iCount++) {
+            int record = (int)drawer.get(iCount);
+            sum += record;
+            if (record > max) {
+                max = record;
             }
-            if (price < min) {
-                min = price;
+            if (record < min) {
+                min = record;
             }
         }
-        avg = sum / products.length;
+        avg = sum / drawer.size();
     }
 }
